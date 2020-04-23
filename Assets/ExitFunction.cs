84e9928b34ddd02363aa28a62dd03df2306fffab
lp4h <lp4h@mtmail.mtsu.edu>
2020-04-23 13:54:59 -0500

    Main menu created

---
+++ Assets/ExitFunction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitFunction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void QuitGame()
+    {
+        Debug.Log("Exit!");
+        Application.Quit();
+    }
+}
